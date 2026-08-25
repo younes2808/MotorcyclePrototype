7905250969a0ebe1277d9399d99c03cb13762bcd
Younes Benhaida <benhaida.mehdi@gmail.com>
2026-08-25 21:20:07 +0200

    Working Suspension

--- Assets/Scripts/MotoController.cs
+++ Assets/Scripts/MotoController.cs
@@ -7,30 +7,50 @@ public class MotoController : MonoBehaviour
     [SerializeField] private Rigidbody bikeRigidbody;
     [SerializeField] private LayerMask drivableLayer;
 
-
     [Header("Constants")]
-    [SerializeField] private float springDamping;
+    [SerializeField] private float springStiffness;
+    [SerializeField] private float damperStiffness;
+    [SerializeField] private float restLength;
+    [SerializeField] private float springTravel;
+    [SerializeField] private float tireRadius;
 
     void Start()
     {
         if (bikeRigidbody == null)
         {
-            //BikeRigidbody wasn't assigned
             Debug.Log("RigidBody wasn't assigned");
             return;
         }
-
     }
 
     private void FixedUpdate()
     {
-
         foreach (Transform wheelTransform in wheelPoint)
         {
-            if (Physics.Raycast(wheelTransform.position, -Vector3.up, out RaycastHit hit))
+            float maxLength = restLength + springTravel;
+
+            if (Physics.Raycast(wheelTransform.position, -wheelTransform.up, out RaycastHit hit, maxLength + tireRadius, drivableLayer))
             {
-                Debug.Log("Found an object - distance: " + hit.distance);
-                Debug.Log(wheelTransform.name + " : " + wheelTransform.position.ToString());
+                Vector3 springDir = wheelTransform.up;
+
+                Vector3 tireWorldVel = bikeRigidbody.GetPointVelocity(wheelTransform.position);
+
+                // Calculate offset from the raycast
+                float offset = restLength - (hit.distance - tireRadius);
+
+                // springDir is a unit vector, so this returns the magnitude of
+                // tireWorldVel projected onto springDir
+                float vel = Vector3.Dot(springDir, tireWorldVel);
+
+                float force = (offset * springStiffness) - (vel * damperStiffness);
+
+                bikeRigidbody.AddForceAtPosition(springDir * force, wheelTransform.position);
+
+                Debug.DrawLine(wheelTransform.position, hit.point, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(wheelTransform.position, -wheelTransform.up * (tireRadius + maxLength), Color.green);
             }
         }
     }
